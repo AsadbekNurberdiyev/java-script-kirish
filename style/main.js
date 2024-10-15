b4d9daa040a1c1ec55abe83d7bd3d46440d33d9d
AsadbekNurberdiyev <asadbeknurberdiyev31@gmail.com>
2024-10-16 01:02:00 +0500

    java script masalalar

---
+++ style/main.js
@@ -0,0 +1,110 @@
+                // 1-masala
+// let a = 5
+// let P = 4 * a
+// console.log(P);
+
+
+// let a = +prompt("sonni kiriting")
+// if(a > 0){
+//     a++;
+// } else if(a<0){
+//    a= a-2;
+// }else {
+//     a=10
+// }
+
+// console.log(a);
+
+            //  2-masala
+// let a=+prompt("kvadratning tomonini kiriting")
+// let S
+//  S=a*a
+//  console.log(S);
+ 
+            //  3-masala
+
+// let a=+prompt("birinchi sonni kiriting")
+// let b=+prompt("ikkinchi sonni kiriting")
+// let S=a*b
+// let P=2*(a+b)
+// console.log("to'rtburchakyuzi " +S+ " peremetri " +P);
+
+        // 4-masala
+// let d=+prompt("aylana diametrini kiriting")
+// const p=3.14
+// let L=p*d 
+// console.log(L);
+
+    //  5-masala
+// let a=+prompt("kubning yon tomonini kiriting")
+// let V=a*a*a
+// console.log("kubning hajmi " +V);
+// let S=6*a*a
+// console.log("kubning sirti yuzi " +S);
+   
+        //  6-masala
+// let a=+prompt("parallellipipedning birinchi tomoni")
+// let b=+prompt("parallellipipedning ikkinchi tomoni")
+// let c=+prompt("parallellipipedning uchinchi tomoni")
+// let V=a*b*c
+// console.log("parallelipiped hajmi " +V);
+// let S=2*(a*b+a*c+b*c)
+// console.log("parallelipiped sirti yuzi "+S);
+ 
+            //  7-masala
+// let r=+prompt("doira radiusi")
+// let p=3.14
+// let L=2*p*r
+// console.log("doira uzunligi "+L);
+// let S=p*r*r
+// console.log("doira yuzi "+S);
+
+                    // 8-masala
+// let a=+prompt("birinchi sonni kiriting")
+// let b=+prompt("ikkinchi sonni kiriting")
+// let P=(a+b)/2
+// console.log(P);
+
+                            //  9-masala
+
+// let a=+prompt("birinchi sonni kiriting")
+// let b=+prompt("ikkinchi sonni kiriting")
+// let S=Math.sqrt(a*b)
+// console.log(S);
+                                    // 10-masala
+
+// let a=+prompt("birinchi sonni kiriting")
+// let b=+prompt("ikkinchi sonni kiriting")
+// let S=a+b
+// console.log("ikki sonning yig'indisi " +S);
+// let P=a*b 
+// console.log("ikki sonning ko'paytmasi " +P);
+// let M=Math.pow(a,2)
+// console.log("birinchi sonning kvadrati " +M);
+// let N=Math.pow(b,2)
+// console.log("ikkinchi sonning kvadrati " +N);
+
+                    //  11-msala
+// let a=+prompt("birinchi sonni kiriting")
+// let b=+prompt("ikkinchi sonni kiriting")
+// let S=a+b
+// console.log("ikki sonning yig'indisi " +S);
+// let P=a*b 
+// console.log("ikki sonning ko'paytmasi " +P);
+// let M=Math.abs(a)
+// console.log("birinchi sonning moduli " +M);
+// let N=Math.abs(b)
+// console.log("ikkinchi sonning moduli " +N);
+ 
+           //  12-masala
+// let a=+prompt("birinchi katetni kiriting")
+// let b=+prompt("ikkinchi katetni kiriting")
+// let c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2))
+// console.log("gipotenuza "+c);
+// let P=a+b+c
+// console.log("peremetr " +P);
+
+                
+
+
+
